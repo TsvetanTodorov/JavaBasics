2a03c5f19d8302a39771af83cce1f36a018932e5
tsvetantodorov <tstodorofff@gmail.com>
2022-10-29 16:36:10 +0300

    Refactoring FishingBoat

--- src/CondiotionalStatementsAdvanced/Exercise/FishingBoat.java
+++ src/CondiotionalStatementsAdvanced/Exercise/FishingBoat.java
@@ -12,54 +12,23 @@ public class FishingBoat {
         int fishermen = Integer.parseInt(scanner.nextLine());
 
         double leftMoney = 0;
-        double rent = 0;
+        double rent;
 
         switch (season) {
             case "Spring":
-                rent = 3000;
-                if (fishermen <= 6) {
-                    rent = rent - (rent * 0.10);
-                } else if (fishermen <= 11) {
-                    rent = rent - (rent * 0.15);
-                } else {
-                    rent = rent - (rent * 0.25);
-                }
-                if (fishermen % 2 == 0) {
-                    rent = rent - (rent * 0.05);
-                }
+                rent = calculateRentAfterDiscount(fishermen, 3000, season);
                 leftMoney = budget - rent;
                 break;
             case "Summer":
             case "Autumn":
-                rent = 4200;
-                if (fishermen <= 6) {
-                    rent = rent - (rent * 0.10);
-                } else if (fishermen <= 11) {
-                    rent = rent - (rent * 0.15);
-                } else {
-                    rent = rent - (rent * 0.25);
-                }
-                if (fishermen % 2 == 0 && !season.equals("Autumn")) {
-                    rent = rent - (rent * 0.05);
-                }
+                rent = calculateRentAfterDiscount(fishermen, 4200, season);
                 leftMoney = budget - rent;
                 break;
             case "Winter":
-                rent = 2600;
-                if (fishermen <= 6) {
-                    rent = rent - (rent * 0.10);
-                } else if (fishermen <= 11) {
-                    rent = rent - (rent * 0.15);
-                } else {
-                    rent = rent - (rent * 0.25);
-                }
-                if (fishermen % 2 == 0) {
-                    rent = rent - (rent * 0.05);
-                }
+                rent = calculateRentAfterDiscount(fishermen, 2600, season);
                 leftMoney = budget - rent;
                 break;
         }
-
 
         if (leftMoney >= 0) {
             System.out.printf("Yes! You have %.2f leva left.", leftMoney);
@@ -68,4 +37,21 @@ public class FishingBoat {
             System.out.printf("Not enough money! You need %.2f leva.", neededMoney);
         }
     }
+
+
+    private static double calculateRentAfterDiscount(int fishermen, double rent, String season) {
+        if (fishermen <= 6) {
+            rent = rent - (rent * 0.10);
+        } else if (fishermen <= 11) {
+            rent = rent - (rent * 0.15);
+        } else {
+            rent = rent - (rent * 0.25);
+        }
+        if (fishermen % 2 == 0 && !season.equals("Autumn")) {
+            rent = rent - (rent * 0.05);
+        }
+        return rent;
+    }
+
+
 }
