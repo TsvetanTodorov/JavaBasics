37668c34e58f6f3045c8fdec5c7deb18a06c2ffa
tsvetantodorov <tstodorofff@gmail.com>
2022-11-02 16:37:08 +0200

    Adding NumbersFromNToOne

---
+++ src/ForLoop/Lab/NumbersFromNToOne.java
@@ -0,0 +1,16 @@
+package ForLoop.Lab;
+
+import java.util.Scanner;
+
+public class NumbersFromNToOne {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        for (int i = num; i >= 1; i--){
+            System.out.println(i);
+        }
+    }
+}
