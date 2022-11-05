1e889e7c1598b3d5d4cc7c091a9efc26750046e0
tsvetantodorov <tstodorofff@gmail.com>
2022-11-05 10:59:55 +0200

    Adding CharacterSequence

---
+++ src/ForLoop/Lab/CharacterSequence.java
@@ -0,0 +1,17 @@
+package ForLoop.Lab;
+
+import java.util.Scanner;
+
+public class CharacterSequence {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        for (int i = 0; i <= text.length() - 1; i++) {
+            char symbol = text.charAt(i);
+            System.out.println(symbol);
+        }
+    }
+}
