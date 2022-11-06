bf153d8034d3a251b21cdb8f3afa45d7160cb5e6
tsvetantodorov <tstodorofff@gmail.com>
2022-11-06 23:00:12 +0200

    Refactoring NumbersEndingInSeven

--- src/ForLoop/Exercise/NumbersEndingInSeven.java
+++ src/ForLoop/Exercise/NumbersEndingInSeven.java
@@ -4,9 +4,13 @@ public class NumbersEndingInSeven {
     public static void main(String[] args) {
 
         for (int i = 7; i <= 997; i++) {
-            if (i % 10 == 7) {
+            if (isEndingInSeven(i)) {
                 System.out.println(i);
             }
         }
+    }
+
+    private static boolean isEndingInSeven(int num) {
+        return num % 10 == 7;
     }
 }
