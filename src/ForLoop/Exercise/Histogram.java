cf3832b81553692dad09304114d7df8c7517ec7f
tsvetantodorov <tstodorofff@gmail.com>
2022-11-06 22:48:25 +0200

    Refactoring Histogram

--- src/ForLoop/Exercise/Histogram.java
+++ src/ForLoop/Exercise/Histogram.java
@@ -18,13 +18,13 @@ public class Histogram {
         for (int i = 1; i <= n; i++) {
             int currentNumber = Integer.parseInt(scanner.nextLine());
 
-            if (currentNumber < 200) {
+            if (isLessThanTwoHundred(currentNumber)) {
                 p1++;
-            } else if (currentNumber < 400) {
+            } else if (isLessThanFourHundred(currentNumber)) {
                 p2++;
-            } else if (currentNumber < 600) {
+            } else if (isLessThanSixHundred(currentNumber)) {
                 p3++;
-            } else if (currentNumber < 800) {
+            } else if (isLessThanEightHundred(currentNumber)) {
                 p4++;
             } else {
                 p5++;
@@ -40,4 +40,22 @@ public class Histogram {
         System.out.println();
         System.out.printf("%.2f%%", (p5 * 1.00) / n * 100);
     }
+
+
+    private static boolean isLessThanTwoHundred(int num) {
+        return num < 200;
+    }
+
+    private static boolean isLessThanFourHundred(int num) {
+        return num < 400;
+    }
+
+    private static boolean isLessThanSixHundred(int num) {
+        return num < 600;
+    }
+
+    private static boolean isLessThanEightHundred(int num) {
+        return num < 800;
+    }
+
 }
